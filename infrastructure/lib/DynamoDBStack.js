5e551a7242fb859bc8c4432887d3bc8117b33154
mo mac <trrl.mo@hotmail.com>
2020-11-22 08:21:50 -0700

    Prefix 'notes' || 'Notes' to existing DynamoDB table constructs; Create a 'Collaborators' table in DynamoDB stack'

--- infrastructure/lib/DynamoDBStack.js
+++ infrastructure/lib/DynamoDBStack.js
@@ -8,20 +8,35 @@ export default class DynamoDBStack extends sst.Stack {
 
     const app = this.node.root;
 
-    const table = new dynamodb.Table(this, "Table", {
+    const notesTable = new dynamodb.Table(this, "NotesTable", {
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST, // Use on-demand billing mode
       sortKey: { name: "noteId", type: dynamodb.AttributeType.STRING },
       partitionKey: { name: "userId", type: dynamodb.AttributeType.STRING },
     });
 
-    // Output values
-    new CfnOutput(this, "TableName", {
-      value: table.tableName,
-      exportName: app.logicalPrefixedName("TableName"),
+    const collaboratorsTable = new dynamodb.Table(this, "CollaboratorsTable", {
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST, // Use on-demand billing mode
+      partitionKey: { name: "userId", type: dynamodb.AttributeType.STRING },
     });
-    new CfnOutput(this, "TableArn", {
-      value: table.tableArn,
-      exportName: app.logicalPrefixedName("TableArn"),
+
+    // Output values
+    new CfnOutput(this, "NotesTableName", {
+      value: notesTable.tableName,
+      exportName: app.logicalPrefixedName("NotesTableName"),
+    });
+    new CfnOutput(this, "NotesTableArn", {
+      value: notesTable.tableArn,
+      exportName: app.logicalPrefixedName("NotesTableArn"),
+    });
+
+    // Output values
+    new CfnOutput(this, "CollaboratorsTableName", {
+      value: collaboratorsTable.tableName,
+      exportName: app.logicalPrefixedName("CollaboratorsTableName"),
+    });
+    new CfnOutput(this, "CollaboratorsTableArn", {
+      value: collaboratorsTable.tableArn,
+      exportName: app.logicalPrefixedName("CollaboratorsTableArn"),
     });
   }
 }
