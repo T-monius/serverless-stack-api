7ae73753fb94977599012f3fedab3c29a141b05c
mo mac <trrl.mo@hotmail.com>
2020-11-22 10:53:34 -0700

    Create a collaborators lib and export a function within for getting collaborators; Import 'getCollaborators' into 'createCollaborators' service

--- services/notes/createCollaborator.js
+++ services/notes/createCollaborator.js
@@ -1,17 +1,19 @@
 import handler from "./libs/handler-lib";
 import dynamoDb from "./libs/dynamodb-lib";
+import getCollaborators from "./libs/collaborators-lib";
 
 export const main = handler(async (event, context) => {
   const data = JSON.parse(event.body);
   const newCollaboratorEmail = data.email;
-  const collaborators = []
-  // TODO: get existing collaborators
-  //   Retrieve userId of new collaborator by email
+  const userId = event.requestContext.identity.cognitoIdentityId;
+  const collaborators = await getCollaborators(userId);
+  // TODO: Retrieve userId of new collaborator by email
   //   Add userId of new collaborator to existing collaborators
+
   const params = {
     TableName: process.env.collaboratorsTableName,
     Item: {
-      userId: event.requestContext.identity.cognitoIdentityId,
+      userId: userId,
       collaborators: collaborators,
       createdAt: Date.now(),
     },
---
+++ services/notes/libs/collaborators-lib.js
@@ -0,0 +1,21 @@
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
+
+const getCollaborators = async (currentUserId) => {
+  const params = {
+    // Potential bug: is process.env available in libs directory?
+    TableName: process.env.collaboratorsTableName,
+    Key: {
+      userId: currentUserId,
+    },
+  };
+
+  const result = await dynamoDb.get(params);
+  if (!result.Item) {
+    throw new Error("Item not found.");
+  }
+
+  return result.Item;
+};
+
+export getCollaborators;
