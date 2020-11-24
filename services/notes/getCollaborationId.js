83a92eb3e0299f17e8aaa31b88b04dabb6bdf669
mo mac <trrl.mo@hotmail.com>
2020-11-24 08:11:07 -0700

    Fix minor build error (i.e. trailing whitespace)

--- services/notes/getCollaborationId.js
+++ services/notes/getCollaborationId.js
@@ -4,6 +4,6 @@ export const main = handler(async (event, context) => {
   const userId = event.requestContext.identity.cognitoIdentityId;
   // TODO: hash the userId
   const userIdForSharing = userId;
-  
+
   return userIdForSharing;
 });
