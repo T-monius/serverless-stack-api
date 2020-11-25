0e85caa46cf6a20fbf7532aa28e70534e8ee3a2a
mo mac <trrl.mo@hotmail.com>
2020-11-25 15:59:46 -0700

    Modify 'list' function to list collaborator's notes as well

--- services/notes/list.js
+++ services/notes/list.js
@@ -1,7 +1,8 @@
 import handler from "./libs/handler-lib";
 import dynamoDb from "./libs/dynamodb-lib";
+import getCollaborators from "./libs/collaborators-lib.js";
 
-export const main = handler(async (event, context) => {
+const listForUser = async (userId) => {
   const params = {
     TableName: process.env.tableName,
     // 'KeyConditionExpression' defines the condition for the query
@@ -11,7 +12,7 @@ export const main = handler(async (event, context) => {
     // 'ExpressionAttributeValues' defines the value in the condition
     // - ':userId': defines 'userId' to be the id of the author
     ExpressionAttributeValues: {
-      ":userId": event.requestContext.identity.cognitoIdentityId,
+      ":userId": ,
     },
   };
 
@@ -19,4 +20,20 @@ export const main = handler(async (event, context) => {
 
   // Return the matching list of items in response body
   return result.Items;
+};
+
+export const main = handler(async (event, context) => {
+  const userId = event.requestContext.identity.cognitoIdentityId;
+  const collaborators = getCollaborators(userId);
+  const items = listForUser(userId);
+
+  await collaborators;
+  await items;
+
+  const allItems = collaborators.reduce( async (items, collaboratorId) => {
+    const collaboratorsItems = await listForUser(collaboratorId);
+    return items.concat(collaboratorsItems);
+  }, items);
+
+  return allItems;
 });
