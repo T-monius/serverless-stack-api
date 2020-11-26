16c7b8ebab5aeeaaa732fbeaa7a454a6158b4d6a
mo mac <trrl.mo@hotmail.com>
2020-11-26 15:01:08 -0700

    Re-implement the 'list' function to its original functionality

--- services/notes/list.js
+++ services/notes/list.js
@@ -1,8 +1,42 @@
 import handler from "./libs/handler-lib";
 import dynamoDb from "./libs/dynamodb-lib";
-import getCollaborators from "./libs/collaborators-lib.js";
+// import getCollaborators from "./libs/collaborators-lib.js";
 
-const listForUser = async (userId) => {
+// const listForUser = async (userId) => {
+//   const params = {
+//     TableName: process.env.tableName,
+//     // 'KeyConditionExpression' defines the condition for the query
+//     // - 'userId = :userId': only return items with matching 'userId'
+//     //   partition key
+//     KeyConditionExpression: "userId = :userId",
+//     // 'ExpressionAttributeValues' defines the value in the condition
+//     // - ':userId': defines 'userId' to be the id of the author
+//     ExpressionAttributeValues: {
+//       ":userId": userId,
+//     },
+//   };
+
+//   const result = await dynamoDb.query(params);
+
+//   // Return the matching list of items in response body
+//   return result.Items;
+// };
+
+export const main = handler(async (event, context) => {
+  // const userId = event.requestContext.identity.cognitoIdentityId;
+  // const collaborators = getCollaborators(userId);
+  // const items = listForUser(userId);
+
+  // await collaborators;
+  // await items;
+
+  // const allItems = collaborators.reduce( async (items, collaboratorId) => {
+  //   const collaboratorsItems = await listForUser(collaboratorId);
+  //   return items.concat(collaboratorsItems);
+  // }, items);
+
+  // return allItems;
+
   const params = {
     TableName: process.env.tableName,
     // 'KeyConditionExpression' defines the condition for the query
@@ -12,7 +46,7 @@ const listForUser = async (userId) => {
     // 'ExpressionAttributeValues' defines the value in the condition
     // - ':userId': defines 'userId' to be the id of the author
     ExpressionAttributeValues: {
-      ":userId": userId,
+      ":userId": event.requestContext.identity.cognitoIdentityId,
     },
   };
 
@@ -20,20 +54,4 @@ const listForUser = async (userId) => {
 
   // Return the matching list of items in response body
   return result.Items;
-};
-
-export const main = handler(async (event, context) => {
-  const userId = event.requestContext.identity.cognitoIdentityId;
-  const collaborators = getCollaborators(userId);
-  const items = listForUser(userId);
-
-  await collaborators;
-  await items;
-
-  const allItems = collaborators.reduce( async (items, collaboratorId) => {
-    const collaboratorsItems = await listForUser(collaboratorId);
-    return items.concat(collaboratorsItems);
-  }, items);
-
-  return allItems;
 });
