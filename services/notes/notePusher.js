33df22e9e3e34b59873fbd72d8dcd5c24d65fabd
mo mac <trrl.mo@hotmail.com>
2020-11-30 14:23:12 -0700

    Trigger a pusher event in 'notePusher' lambda; include logging

--- services/notes/notePusher.js
+++ services/notes/notePusher.js
@@ -1,7 +1,20 @@
 import handler from "./libs/handler-lib";
+import Pusher from "pusher";
 
 export const main = handler(async (event, context) => {
   // const data = JSON.parse(event.body);
   // const userId = event.Records[0].dynamodb.Keys.userId.S;
+  console.log("Pusher: ", Pusher);
+  const pusher = new Pusher({
+    appId: "1114436",
+    key: "71006dcae7db25deafa6",
+    secret: "1335d49a4dff871c76fa",
+    cluster: "us2",
+    useTLS: true
+  });
 
+  console.log("pusher: ", pusher);
+  pusher.trigger("ridgeline-notes", "note-table-change", {
+    message: "'notePusher' lambda!"
+  });
 });
